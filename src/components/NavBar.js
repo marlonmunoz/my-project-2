6eb1d63d9a1d73266f4274e9cc23a44b9e256576
Marlon Munoz <marlon@marlonshade.com>
2024-08-16 13:12:49 -0400

    json items updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,29 @@ import { NavLink } from "react-router-dom";
 function NavBar () {
     return (
         <nav className="nav-tabs">
-            <NavLink to="/">Home </NavLink>
-            <NavLink to="/add_toy"> AddToy </NavLink>
-            <NavLink to="/about">About </NavLink>
+            <ul>
+                <li className="my-links">
+                    <NavLink to="/">Home </NavLink>
+                </li>
+                <li className="my-links">
+                    <NavLink to="/add_toy"> AddToy </NavLink>
+                </li>
+                <li className="my-links">
+                    <NavLink to="/about">About </NavLink>
+                </li>
+            </ul>
         </nav>
     )
 }
 export default NavBar;
+
+
+// function NavBar () {my-links
+//     return (
+//         <nav className="nav-tabs">
+//             <NavLink to="/">Home </NavLink>
+//             <NavLink to="/add_toy"> AddToy </NavLink>
+//             <NavLink to="/about">About </NavLink>
+//         </nav>
+//     )
+// }
--- src/components/NewToyForm.js
+++ src/components/NewToyForm.js
@@ -4,9 +4,13 @@ function NewToyForm() {
 
     const navigate = useNavigate();
 
-    navigate('/')
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        navigate('/');  // Redirect to the home page
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <h3>Add a new toy 🧸 :</h3>
             <label htmlFor="name">Toy Name: </label>
             <input type="text" id="name" name="name" />
